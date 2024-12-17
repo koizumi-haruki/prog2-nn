09d59ac4a7e4b43537c673ef77b1ca6ca8d7ccc3
Koizumi Haruki <j3327vpa@sit.ac.jp>
2024-12-17 16:35:50 +0900

    デバイス転送バグの修正

--- models.py
+++ models.py
@@ -24,12 +24,12 @@ class MyModel(nn.Module):
 def test_accuracy(model, dataloader, device='cpu'):
     n_corrects=0
 
-    model.to(device)
+    model = model.to(device)
 
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
 
         with torch.no_grad():
             logits_batch = model(image_batch)
@@ -44,11 +44,11 @@ def test_accuracy(model, dataloader, device='cpu'):
 
 def train(model, dataloader, loss_fn, optimizer, device='cpu'):
     """1 epoch の学習を行う"""
-    model.to(device)
+    model = model.to(device)
     model.train()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
 
         logits_batch = model(image_batch)
 
@@ -65,12 +65,12 @@ def train(model, dataloader, loss_fn, optimizer, device='cpu'):
 def test(model, dataloader, loss_fn, device='cpu'):
     loss_total = 0.0
     
-    model.to(device)
+    model = model.to(device)
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
-        
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
+
         with torch.no_grad():
             logits_batch = model(image_batch)
         
--- train.py
+++ train.py
@@ -45,7 +45,7 @@ for image_batch, label_batch in dataloader_test:
     break
 
 model = models.MyModel()
-acc_test = models.test_accuracy(model, dataloader_test)
+acc_test = models.test_accuracy(model, dataloader_test, device=device)
 print(f'test_accuracy: {acc_test*100:.3f}%')
 
 model = models.MyModel()
@@ -55,8 +55,8 @@ loss_fn = torch.nn.CrossEntropyLoss()
 learning_rate = 0.003
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-acc_test = models.test_accuracy(model, dataloader_test)
-print(f'test accuracy: {acc_test*100:.2f}%')
+acc_train = models.test_accuracy(model, dataloader_test, device=device)
+print(f'test accuracy: {acc_train*100:.2f}%')
 
 n_epochs = 5
 
@@ -70,28 +70,28 @@ for k in range(n_epochs):
     print(f'epoch {k+1}/{n_epochs}', end=': ', flush=True)
 
     time_start = time.time()
-    loss_train = models.train(model, dataloader_train, loss_fn, optimizer)
+    loss_train = models.train(model, dataloader_train, loss_fn, optimizer, device=device)
     print(f'train loss: {loss_train:.3f}', end=', ')
     loss_train_history.append(loss_train)
     time_end = time.time()
     print(f'実行時間: {time_end-time_start:.2f}秒', end=', ', flush=True)
 
     time_start = time.time()
-    loss_test = models.test(model, dataloader_test, loss_fn)
+    loss_test = models.test(model, dataloader_test, loss_fn, device=device)
     print(f'test loss: {loss_test:.3f}', end=', ')
     loss_test_history.append(loss_test)
     time_end = time.time()
     print(f'実行時間: {time_end-time_start:.2f}秒', end=', ', flush=True)
     
     time_start = time.time()
-    acc_train = models.test_accuracy(model, dataloader_train)
+    acc_train = models.test_accuracy(model, dataloader_train, device=device)
     print(f'train accuracy: {acc_train*100:.3f}%', end=', ')
     acc_train_history.append(acc_train)
     time_end = time.time()
     print(f'実行時間: {time_end-time_start:.2f}秒', end=', ', flush=True)
 
     time_start = time.time()
-    acc_test = models.test_accuracy(model, dataloader_test)
+    acc_test = models.test_accuracy(model, dataloader_test, device=device)
     print(f'test_accuracy: {acc_test*100:.3f}%', end=', ', flush=True)
     acc_test_history.append(acc_test)
     time_end = time.time()
