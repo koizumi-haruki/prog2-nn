53b1b1cc14ffcaafb0cee2bea84cb0e1c9ff1a0d
Koizumi Haruki <j3327vpa@sit.ac.jp>
2024-12-17 16:16:24 +0900

    deviceを指定出来るように変更

--- models.py
+++ models.py
@@ -21,11 +21,16 @@ class MyModel(nn.Module):
         logits = self.network(x)
         return logits
     
-def test_accuracy(model, dataloader):
+def test_accuracy(model, dataloader, device='cpu'):
     n_corrects=0
+
+    model.to(device)
 
     model.eval()
     for image_batch, label_batch in dataloader:
+        image_batch.to(device)
+        label_batch.to(device)
+
         with torch.no_grad():
             logits_batch = model(image_batch)
         
@@ -37,10 +42,14 @@ def test_accuracy(model, dataloader):
 
     return accuracy
 
-def train(model, dataloader, loss_fn, optimizer):
+def train(model, dataloader, loss_fn, optimizer, device='cpu'):
     """1 epoch の学習を行う"""
+    model.to(device)
     model.train()
     for image_batch, label_batch in dataloader:
+        image_batch.to(device)
+        label_batch.to(device)
+
         logits_batch = model(image_batch)
 
         loss = loss_fn(logits_batch, label_batch)
@@ -53,11 +62,15 @@ def train(model, dataloader, loss_fn, optimizer):
     
     return loss.item()
 
-def test(model, dataloader, loss_fn):
+def test(model, dataloader, loss_fn, device='cpu'):
     loss_total = 0.0
-
+    
+    model.to(device)
     model.eval()
     for image_batch, label_batch in dataloader:
+        image_batch.to(device)
+        label_batch.to(device)
+        
         with torch.no_grad():
             logits_batch = model(image_batch)
         
